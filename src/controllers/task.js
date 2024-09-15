51c7a443d148870b12e346dac16666932882451e
Kássio Sá <kassio.slva@gmail.com>
2024-09-15 17:20:19 -0300

    style: format code

--- src/controllers/task.js
+++ src/controllers/task.js
@@ -1,23 +1,23 @@
 import { Task } from '../models/Task.js'
 
 export async function createTask(req, res) {
-    try {
-      const task = await Task.create({ ...req.body, author: req.user.id })
-  
-      return res.status(201).json(task)
-    } catch (error) {
-      console.log(error)
-      return res.status(500).json({ message: 'Erro ao criar tarefa' })
-    }
+  try {
+    const task = await Task.create({ ...req.body, author: req.user.id })
+
+    return res.status(201).json(task)
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ message: 'Erro ao criar tarefa' })
   }
+}
 
 export async function getTasks(req, res) {
   try {
-     const task = await Task.find({ author: req.user.id })
-  
+    const task = await Task.find({ author: req.user.id })
+
     return res.status(200).json(task)
- } catch (error) {
-   console.log(error)
-   return res.status(500).json({ message: 'Erro ao buscar tarefas' })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ message: 'Erro ao buscar tarefas' })
   }
 }
